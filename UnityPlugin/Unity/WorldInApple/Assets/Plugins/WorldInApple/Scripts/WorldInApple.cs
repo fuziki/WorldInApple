ccd90f052ce985af92fbebfac2599623a34fe8ce
fuziki <hymne.shannon.projects@gmail.com>
2020-02-19 00:34:24 +0900

    dioまで作った

--- UnityPlugin/Unity/WorldInApple/Assets/Plugins/WorldInApple/Scripts/WorldInApple.cs
+++ UnityPlugin/Unity/WorldInApple/Assets/Plugins/WorldInApple/Scripts/WorldInApple.cs
@@ -75,11 +75,34 @@ namespace WorldInApplePlugin {
         public readonly int f0_length;
         public readonly int fft_size;
 
-        private AllocableArray tmp_f0;
-        private AllocableArray f0;
-        private AllocableArray time_axis;
-        private AllocableArray spectrogram;
-        private AllocableArray aperiodicity;
+        private AllocableArray alloable_x;
+        private AllocableArray alloable_y;
+        private AllocableArray alloable_tmp_f0;
+        private AllocableArray alloable_f0;
+        private AllocableArray alloable_time_axis;
+        private AllocableArray alloable_spectrogram;
+        private AllocableArray alloable_aperiodicity;
+
+        public IntPtr x
+        { get { return alloable_x.ArrayPtr; } }
+
+        public IntPtr y
+        { get { return alloable_y.ArrayPtr; } }
+
+        public IntPtr tmp_f0
+        { get { return alloable_tmp_f0.ArrayPtr; } }
+
+        public IntPtr f0
+        { get { return alloable_f0.ArrayPtr; } }
+
+        public IntPtr time_axis
+        { get { return alloable_time_axis.ArrayPtr; } }
+
+        public IntPtr spectrogram
+        { get { return alloable_spectrogram.ArrayPtr; } }
+
+        public IntPtr aperiodicity
+        { get { return alloable_aperiodicity.ArrayPtr; } }
 
         public Parameters(int fs, double frame_period, int x_length)
         {
@@ -89,16 +112,28 @@ namespace WorldInApplePlugin {
 
             this.f0_length = GetSamplesForDIO(fs, x_length, frame_period);
 
-            this.time_axis = new AllocableArray(f0_length);
-            this.f0 = new AllocableArray(f0_length);
-            this.tmp_f0 = new AllocableArray(f0_length);
 
             var cheapTrickOption = make_CheapTrickOption(fs);
             this.fft_size = GetFFTSizeForCheapTrick(fs, cheapTrickOption);
             destroy_CheapTrickOption(cheapTrickOption);
 
-            this.spectrogram = new AllocableArray(f0_length, fft_size / 2 + 1);
-            this.aperiodicity = new AllocableArray(f0_length, fft_size / 2 + 1);
+            this.alloable_x = new AllocableArray(x_length);
+            this.alloable_y = new AllocableArray(x_length);
+            this.alloable_tmp_f0 = new AllocableArray(f0_length);
+            this.alloable_f0 = new AllocableArray(f0_length);
+            this.alloable_time_axis = new AllocableArray(f0_length);
+            this.alloable_spectrogram = new AllocableArray(f0_length, fft_size / 2 + 1);
+            this.alloable_aperiodicity = new AllocableArray(f0_length, fft_size / 2 + 1);
+        }
+        ~Parameters()
+        {
+            alloable_x = null;
+            alloable_y = null;
+            alloable_tmp_f0 = null;
+            alloable_f0 = null;
+            alloable_time_axis = null;
+            alloable_spectrogram = null;
+            alloable_aperiodicity = null;
         }
     }
 
@@ -111,6 +146,12 @@ namespace WorldInApplePlugin {
 
         [DllImport(dllName)]
         private static extern void destroy_DioOption(IntPtr option);
+
+        [DllImport(dllName)]
+        private static extern void Dio(IntPtr x, int x_length, int fs, IntPtr option, IntPtr temporal_positions, IntPtr f0);
+
+        [DllImport(dllName)]
+        private static extern void StoneMask(IntPtr x, int x_length, int fs, IntPtr temporal_positions, IntPtr f0, int f0_length, IntPtr refined_f0);
 
         private IntPtr option;
         private Parameters parameters;
@@ -129,7 +170,8 @@ namespace WorldInApplePlugin {
 
         public void EstimateF0()
         {
-
+            Dio(parameters.x, parameters.x_length, parameters.fs, option, parameters.time_axis, parameters.tmp_f0);
+            StoneMask(parameters.x, parameters.x_length, parameters.fs, parameters.time_axis, parameters.tmp_f0, parameters.f0_length, parameters.f0);
         }
     }
 }
